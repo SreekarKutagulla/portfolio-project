6bc143e7bc25e23545ff518ee6aca52b7ccbe3ea
SK <sreekar2025@gmail.com>
2024-10-28 20:55:11 -0400

    Kernel

---
+++ src/components/worm/WormKernel.java
@@ -0,0 +1,15 @@
+package components.worm;
+
+import components.standard.Standard;
+
+public interface WormKernel extends Standard<WormKernel> {
+
+    boolean isMutated();
+
+    boolean isSleep();
+
+    void set(int var1, String var2);
+
+    void get(int var3);
+
+}
